a472667dbd9eb4351b8cff1d0511817dcec826ab
nancylicona <32726663+nancylicona@users.noreply.github.com>
2019-03-13 05:24:35 -0500

    Update color.js

--- skills/color.js
+++ skills/color.js
@@ -4,25 +4,14 @@
 module.exports = function (controller) {
     
 
-    controller.hears([/^favorit/], 'direct_message,direct_mention', function (bot, message) {
-   "name": "addTodo",
-   "favorite": [],
-   "parameters": [],
-   "response": {
-         "type": "mb",
-         "value": "add"
-    }
+    controller.hears([/^favorite/], 'direct_message,direct_mention', function (bot, message) {
         bot.startConversation(message, function (err, convo) {
-
+            
             convo.ask('I see you have a new favorite item/food. What item/food is it?', function (response, convo) {
                 convo.say("Cool, I added '" + response.text + "' to your list of favorites!");
                 myObj.favorite[i]=response.text;
              }   
            convo.say("Displaying your list of favorites:");        
-            for (i in myObj.favorite) {
-                x = myObj.favorite[i];
-                console.log(x);
-            }
            convo.next();            
                 
 
