e3769e6ce38c3cd12a27a37d97654ac77f63c46a
nancylicona <32726663+nancylicona@users.noreply.github.com>
2019-03-13 10:54:57 -0500

    Update help.js

--- skills/help.js
+++ skills/help.js
@@ -5,8 +5,8 @@ module.exports = function (controller) {
 
     controller.hears([/^help$/], 'direct_message,direct_mention', function (bot, message) {
         var text = "Here are my skills:";
-        text += "\n- " + bot.appendMention(message, "recipe") + ": shows recipes for meals from different websites";
-        text += "\n- " + bot.appendMention(message, "drinks") + ": let you store your favorite meal of the day";
+        text += "\n- " + bot.appendMention(message, "recipe") + ": shows recipes for meals from the user's favorite websites";
+        text += "\n- " + bot.appendMention(message, "drinks") + ": shows recipes for drinks from the user's favorite websites";
         text += "\n\nI also understand:";
         text += "\n- " + bot.appendMention(message, "about") + ": shows metadata about myself";
         text += "\n- " + bot.appendMention(message, "help") + ": spreads the word about my skills";
