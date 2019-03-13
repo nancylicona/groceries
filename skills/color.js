21cc250f5962ac6b33ab5a614809babcf92a8f16
nancylicona <32726663+nancylicona@users.noreply.github.com>
2019-03-13 05:19:20 -0500

    Update color.js

--- skills/color.js
+++ skills/color.js
@@ -2,14 +2,30 @@
 // Simplest use of Botkit's conversation system
 //
 module.exports = function (controller) {
+    
 
     controller.hears([/^favorit/], 'direct_message,direct_mention', function (bot, message) {
-
+   "name": "addTodo",
+   "favorite": [],
+   "parameters": [],
+   "response": {
+         "type": "mb",
+         "value": "add"
+    }
         bot.startConversation(message, function (err, convo) {
 
-            convo.ask('I see you have a new favorite item/food. What I item/food is it?', function (response, convo) {
+            convo.ask('I see you have a new favorite item/food. What item/food is it?', function (response, convo) {
                 convo.say("Cool, I added '" + response.text + "' to your list of favorites!");
-                convo.next();
+                myObj.favorite[i]=response.text;
+             }   
+           convo.say("Displaying your list of favorites:");        
+            for (i in myObj.favorite) {
+                x = myObj.favorite[i];
+                console.log(x);
+            }
+           convo.next();            
+                
+
             });
         });
 
