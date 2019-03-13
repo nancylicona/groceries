28e00b37ae3de174500ead0781939edae6a8a426
nancylicona <32726663+nancylicona@users.noreply.github.com>
2019-03-13 05:30:54 -0500

    Update color.js

--- skills/color.js
+++ skills/color.js
@@ -2,17 +2,15 @@
 // Simplest use of Botkit's conversation system
 //
 module.exports = function (controller) {
-    
 
-    controller.hears([/^favorite/], 'direct_message,direct_mention', function (bot, message) {
+    controller.hears([/^color$/], 'direct_message,direct_mention', function (bot, message) {
+
         bot.startConversation(message, function (err, convo) {
-            
-            convo.ask('I see you have a new favorite item/food. What item/food is it?', function (response, convo) {
-                convo.say("Cool, I added '" + response.text + "' to your list of favorites!");
-                convo.say("Displaying your list of favorites:");        
-                convo.next();   
-             }     
+            convo.say('This is a Botkit conversation sample.');
 
+            convo.ask('What is your favorite color?', function (response, convo) {
+                convo.say("Cool, I like '" + response.text + "' too!");
+                convo.next();
             });
         });
 
