7330ba91a0a180521cfdbdd08786367a458df41b
nancylicona <32726663+nancylicona@users.noreply.github.com>
2019-03-13 10:10:12 -0500

    Update variables.js

--- skills/variables.js
+++ skills/variables.js
@@ -1,60 +1,93 @@
 //
-// Threaded conversation where variables are set to store user choices
+// Example of a conversation with a menu that loops until explicitly stopped
 //
 module.exports = function (controller) {
 
-    controller.hears([/^variables$/], "direct_message,direct_mention", function (bot, message) {
+    controller.hears([/^recipe/], 'direct_message,direct_mention', function (bot, message) {
 
         bot.startConversation(message, function (err, convo) {
 
-            convo.ask("What was your favorite meal today?", [
+            var question = "Here are a few proposed recipe websites:";
+            question += "<br/> `1)` Smoothie options (**smoothies**)";
+            question += "<br/> `2)` Coffee options (**coffee**)";
+            question += "<br/> `3)` Boba tea options (**boba**)";
+            question += "<br/> `4)` Healthy options (**healthy**)";             
+            question += "\n\nWhat do you want to check?<br/>_(type a number, a **bold keyword** or `stop`)_";
+            convo.ask(question, [
                 {
-                    pattern: "^breakfast|lunch|dinner|snack|dessert",
+                    pattern: "1|smoothies|smoothie",
                     callback: function (response, convo) {
-                        convo.gotoThread("confirm_choice");
+                        convo.gotoThread('menu_1');
                     },
-                },
-                {
+                }
+                , {
+                    pattern: "2|coffee|coffees",
+                    callback: function (response, convo) {
+                        convo.gotoThread('menu_2');
+                    },
+                }
+                , {
+                    pattern: "3|boba|bobas|bobatea|tea",
+                    callback: function (response, convo) {
+                        convo.gotoThread('menu_3');
+                    },
+                }
+                , {
+                    pattern: "4|healthy",
+                    callback: function (response, convo) {
+                        convo.gotoThread('menu_4');
+                  },
+                }
+                , {
+                    pattern: "cancel|stop",
+                    callback: function (response, convo) {
+                        convo.gotoThread('action_cancel');
+                    },
+                }
+                , {
                     default: true,
                     callback: function (response, convo) {
                         convo.gotoThread('bad_response');
                     }
                 }
-            ], { key: "answer" });
+            ]);
 
+            // Menu option 1)
+            convo.addMessage({
+                text: "Alright! Time for a [smoothie](https://www.foodnetwork.com/recipes/articles/50-smoothies). Check the online recipes and pick your favorite!",
+                action: 'default'
+            }, 'menu_1');
+
+            // Menu option 2)
+            convo.addMessage({
+                text: "But first! [Coffee](https://www.allrecipes.com/recipes/134/drinks/coffee/) from your favorite website. Check the online recipes and pick your favorite!",
+                action: 'default'
+            }, 'menu_2');
+
+            // Menu option 3)
+            convo.addMessage({
+                text: "[Boba tea time](https://www.bubbleteasupply.com/bubble-tea-recipes/). Check the online recipes and pick your favorite!",
+                action: 'default'
+            }, 'menu_3');
+
+             // Menu option 4)
+            convo.addMessage({
+                 text: "Lets be healthy today! [Healthy options](http://www.weightloss.com.au/healthy-recipes/drink-recipes/). Check the online recipes and pick your favorite!",
+                action: 'default'
+            }, 'menu_4');                    
+            
+            // Cancel
+            convo.addMessage({
+                text: "Got it, cancelling...",
+                action: 'stop', // this marks the converation as unsuccessful
+            }, 'action_cancel');
 
             // Bad response
             convo.addMessage({
-                text: "Sorry, I don't know this color.<br/>_Tip: try breakfast, lunch, dinner, snack or dessert!_",
+                text: "Sorry, I did not understand.",
                 action: 'default',
             }, 'bad_response');
 
-            // Confirmation thread
-            convo.addMessage(
-                "You picked '{{responses.answer}}'",
-                "confirm_choice");
-
-            convo.addQuestion("Please, confirm your choice ? (yes|no)", [
-                {
-                    pattern: "^yes|hey|oui|si|da$",
-                    callback: function (response, convo) {
-                        var pickedColor = convo.extractResponse('answer');
-                        convo.setVar("color", pickedColor);
-                        convo.gotoThread("success");
-                    },
-                },
-                {
-                    default: true,
-                    callback: function (response, convo) {
-                        convo.transitionTo("default", "Got it, let's try again...");
-                    }
-                }
-            ], {}, "confirm_choice");
-
-            // Success thread
-            convo.addMessage(
-                "Cool, I love '{{vars.color}}' too! Remember the recipe for later",
-                "success");
         });
     });
 };
